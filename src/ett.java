dbeb936f81d57e2532862b563c41fe684d841a3c
BestRestoShamanEU <samuelhenriksson8@hotmail.com>
2018-09-28 14:19:33 +0200

    Lektionsuppgift 1

---
+++ src/ett.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ett
+{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+	int x = input.nextInt();
+	int y = input.nextInt();
+	
+	
+	
+	if(y<x) { 
+		System.out.println("min" + y);
+			}
+	
+	else if(x<y) {
+		
+		System.out.println("min" + x);
+	}
+	
+}
+	
+}
